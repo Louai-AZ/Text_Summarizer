c6caba68e9cbf3243820fafc509d02d1db3d0754
Louai-AZ <azouni.louai@gmail.com>
2023-09-28 09:55:14 +0100

    project structure added

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging   # To log all infos during runtime
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github.com/workflows/.gitkeep",   # i'll use it whenever i'll be doing the cicd deployment (yaml file) / whenever i commit my code in github ,it'll automatically take the code to the cloud 
+                                        # i create gitkeep file because when commiting to github , empty folder'll not be uploaded 
+    f"src/{project_name}/__init__.py",  # So that i can import files from folders 
+    f"src/{project_name}/components/__init__.py", 
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py", # Inside this file, i'll write All my utility
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml", # Where i'm going to keep all my model parameters
+    "app.py",
+    "main.py",
+    "Dockerfile", # i'll build one docker image of our source code and do the deployment of that image to EC2
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb", # will cotain all the notebook experiments
+    ]
+
+
+for filepath in list_of_files : 
+    filepath = Path(filepath) # To provide the path based on the OS i'm using 
+    filedir , filename = os.path.split(filepath) 
+    
+    if filedir != "" : 
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0) :
+        with open(filepath, 'w') as f:
+            pass 
+            logging.info(f"Creating empty file: {filepath}")
+    else :
+        logging.info(f"{filename} is already exists")
