67d30285ee6782ec501432042d8b6d539faa6352
Louai-AZ <azouni.louai@gmail.com>
2023-10-01 10:34:42 +0100

    Model Trainer & Evaluation done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.Step1_data_ingestion import DataIngestionTrainingPi
 from textSummarizer.pipeline.Step2_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.Step3_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.Step4_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.Step5_model_evaluation import ModelEvaluationTrainingPipeline
 
 from textSummarizer.logging import logger
 
@@ -40,14 +41,27 @@ except Exception as e:
         logger.exception(e)
         raise e
     
+
     
 STAGE_NAME = "Model Trainer stage"
 try: 
     logger.info(f"*******************")
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> Step 4 : {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> Step 4 : {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Model Evaluation stage"
+try: 
+    logger.info(f"*******************")
+    logger.info(f">>>>>> Step 5 : {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>> Step 5 : {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
--- src/textSummarizer/pipeline/Step4_model_trainer.py
+++ src/textSummarizer/pipeline/Step4_model_trainer.py
@@ -7,6 +7,7 @@ class ModelTrainerTrainingPipeline:
     def __init__(self):
         pass
 
+
     def main(self):
         config = ConfigurationManager()
         model_trainer_config = config.get_model_trainer_config()
