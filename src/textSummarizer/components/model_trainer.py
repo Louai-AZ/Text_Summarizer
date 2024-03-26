cc079747c3c204962908c10706c8fe67ab0fd12f
Louai-AZ <azouni.louai@gmail.com>
2024-03-26 21:45:28 +0000

    model trainer component updated

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -15,12 +15,12 @@ class ModelTrainer:
     
     def train(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
+        print("-"*50 ,device , "-"*50 )
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
         dataset_samsum_pt = load_from_disk(self.config.data_path)
-
         trainer_args = TrainingArguments(
             output_dir=self.config.root_dir, num_train_epochs=self.config.num_train_epochs, warmup_steps=self.config.warmup_steps,
             per_device_train_batch_size=self.config.per_device_train_batch_size, per_device_eval_batch_size=self.config.per_device_train_batch_size,
@@ -28,19 +28,11 @@ class ModelTrainer:
             evaluation_strategy=self.config.evaluation_strategy, eval_steps=self.config.eval_steps, save_steps=1e6,
             gradient_accumulation_steps=self.config.gradient_accumulation_steps
         ) 
-
-
-        # trainer_args = TrainingArguments(
-        #     output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
-        #     per_device_train_batch_size=1, per_device_eval_batch_size=1,
-        #     weight_decay=0.01, logging_steps=10,
-        #     evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
-        #     gradient_accumulation_steps=16
-        # ) 
+        
 
         trainer = Trainer(model=model, args=trainer_args,
                 tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                train_dataset=dataset_samsum_pt["train"], 
+                train_dataset=dataset_samsum_pt["train"],
                 eval_dataset=dataset_samsum_pt["validation"])
         
         trainer.train()
--- template.py
+++ template.py
@@ -1,18 +1,21 @@
 import os
 from pathlib import Path
-import logging   # To log all infos during runtime
+import logging
+
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+
 project_name = "textSummarizer"
+
 
 list_of_files = [
     ".github.com/workflows/.gitkeep",   # i'll use it whenever i'll be doing the cicd deployment (yaml file) / whenever i commit my code in github ,it'll automatically take the code to the cloud 
                                         # i create gitkeep file because when commiting to github , empty folder'll not be uploaded 
-    f"src/{project_name}/__init__.py",  # So that i can import files from folders 
+    f"src/{project_name}/__init__.py",  # Constructur file
     f"src/{project_name}/components/__init__.py", 
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py", # Inside this file, i'll write All my utility
+    f"src/{project_name}/utils/common.py", # Inside this file, i'll write a ll my utility
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
@@ -26,7 +29,7 @@ list_of_files = [
     "Dockerfile", # i'll build one docker image of our source code and do the deployment of that image to EC2
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb", # will cotain all the notebook experiments
+    "notebooks/trials.ipynb",
     ]
 
 
